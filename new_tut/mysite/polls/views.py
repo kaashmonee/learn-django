e88e38b49c8226c86614e8acfdefa11cef0b4675
skanda <skandakk@gmail.com>
2018-05-24 15:41:54 -0500

    added some documentation

--- new_tut/mysite/polls/views.py
+++ new_tut/mysite/polls/views.py
@@ -42,6 +42,34 @@ def results(request, question_id):
     response = "You're looking at the results of question %s."
     return HttpResponse(response % question_id)
 
+# Some shit about this vote function
+# request.POST is a dictionary-like object that lets you access submitted data by key name. 
+# In this case, request.POST['choice'] returns the ID of the selected choice, as a string. 
+# request.POST values are always strings.
+
+# Note that Django also provides request.GET for accessing GET data in the same 
+# way – but we’re explicitly using request.POST in our code, to ensure that data 
+# is only altered via a POST call.
+
+# request.POST['choice'] will raise KeyError if choice wasn’t provided in POST data. 
+# The above code checks for KeyError and redisplays the question form with an error message 
+# if choice isn’t given.
+
+# After incrementing the choice count, the code returns an HttpResponseRedirect 
+# rather than a normal HttpResponse. HttpResponseRedirect takes a single argument: 
+# the URL to which the user will be redirected (see the following point for how 
+# we construct the URL in this case).
+
+# As the Python comment above points out, you should always return an HttpResponseRedirect 
+# after successfully dealing with POST data. This tip isn’t specific to Django; it’s 
+# just good Web development practice.
+
+# We are using the reverse() function in the HttpResponseRedirect constructor in this example. 
+# This function helps avoid having to hardcode a URL in the view function. 
+# It is given the name of the view that we want to pass control to and the variable 
+# portion of the URL pattern that points to that view. In this case, using the URLconf 
+# we set up in Tutorial 3, this reverse() call will return a string like
+
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
