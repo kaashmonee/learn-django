88f317b23cf1ea3388984125d7ef416a20438568
skanda <skandakk@gmail.com>
2018-05-26 18:24:41 -0500

    adding everything, rmeoving stuff, untracking stuff, and updating .gitignore

--- django-sentdex/mysite/personal/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def index(request):
-    # returns and renders the home.html file
-    return render(request, 'personal/home.html')
