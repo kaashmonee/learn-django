eae5d40dd6a4220475cd9d69698a016fbf690aef
skanda <skandakk@gmail.com>
2018-05-29 06:46:36 -0400

    more minor changes

--- learnchannels/channelsite/chat/routing.py
+++ learnchannels/channelsite/chat/routing.py
@@ -4,5 +4,7 @@ from django.conf.urls import url
 from . import consumers
 
 websocket_urlpatterns = [
+    # Some regex that I don't really know what it does ATM
+    # Looks like it tells people to go to the consumers.ChatConsumer class
     url(r'^ws/chat/(?P<room_name>[^/]+)/$', consumers.ChatConsumer),
 ]
--- learnchannels/channelsite/chat/urls.py
+++ learnchannels/channelsite/chat/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns= [
+    # Routes to index and room name
     url(r'^$', views.index, name="index"),
     url(r'^(?P<room_name>[^/]+)/$', views.room, name="room"),
 ]
